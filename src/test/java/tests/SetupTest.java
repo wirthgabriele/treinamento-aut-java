c98bd7bb5065d1da8cd59ebaba04ed33128d83bf
Gabriele Wirth <wirthgabrielew@gmail.com>
2021-10-07 10:46:51 -0300

    segundo commit

--- src/test/java/tests/SetupTest.java
+++ src/test/java/tests/SetupTest.java
@@ -1,16 +1,41 @@
 package tests;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import utils.Browser;
 import utils.Utils;
 
 import static org.junit.Assert.assertTrue;
 
 public class SetupTest extends BaseTests{
+    Browser browser = new Browser();
 
     @Test
     public void testOpeningBrowserAndLoadingPage(){
         assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl()));
         System.out.println("Abrimos o navegador e carregamos a url!");
     }
+
+    @Test
+    public void testLogin(){
+        Browser.getCurrentDriver().findElement(By.className("login")).click();
+        //browser.wait.until(ExpectedConditions.elementToBeClickable(By.className("login")));
+        System.out.println("Clicou em Sign In e direcionou para a página de login");
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl()
+                .contains((Utils.getBaseUrl().concat("index.php?controller=authentication&back=my-account"))));
+        Browser.getCurrentDriver().findElement(By.id("email")).sendKeys("rrsetcwi+autopractice@gmail.com");
+        System.out.println("Preencheu o email");
+        Browser.getCurrentDriver().findElement(By.id("passwd")).sendKeys("teste123");
+        System.out.println("Preencheu a senha");
+        Browser.getCurrentDriver().findElement(By.id("SubmitLogin")).click();
+        System.out.println("Clicou em Sign In");
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl().concat("index.php?controller=my-account")));
+        System.out.println("Validou a url de minha conta");
+        assertTrue(Browser.getCurrentDriver().findElement(By.className("page-heading")).getText().contains("MY ACCOUNT"));
+        System.out.println("Validou Minha Conta no site");
+
+    }
 }
+
+
--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -3,6 +3,6 @@ package utils;
 public class Utils {
 
     public static String getBaseUrl(){
-        return "http://automationpractice.com/*";
+        return "http://automationpractice.com/";
     }
 }
